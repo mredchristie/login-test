89621fee6e15f11aa9ce8dd3a1842d2a173a165d
mredchristie <45788302+mredchristie@users.noreply.github.com>
2022-01-28 11:51:06 +0000

    updated homepage

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 
-
-
-
-
-<button type="button" class="btn btn-info"><a href="sign-up.php" >Sign up here</a></button>
-<button type="button" class="btn btn-info"><a href="login.php" >Login Here</a></button>
-
-
-
-
-
-
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home Page</a>
+    <a class="navbar-brand" href="sign-up.php">Sign up</a>
+    <a class="navbar-brand" href="login.php">Login</a>
+    <a class="navbar-brand" href="logout.php">Logout</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>
+  </div>
+</nav>
 
 <?php
-
-
-
 
 session_start();
 
@@ -31,13 +30,9 @@ if ( isset($_SESSION['username']))
 }
 
 
-
-
-
 ?>
 
-Click here to <a href="logout.php" tite="Logout">Logout.</a>
 
 
-
+</body>
 
